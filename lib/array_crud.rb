29622cb49974a8845c8e5fca266bc2486a960b85
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-24 07:30:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [0, 1, 2, 3]
   array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  [0, 1, 2, 3]
   array.unshift("wow")
 end
 
